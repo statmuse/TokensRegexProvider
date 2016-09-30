03611b7d705ffe651a5cde174d004be915d05b8b
Sergey Tihon <sergey.tihon@gmail.com>
2016-09-30 23:16:02 +0300

    advanced sample with pattern over pattern in custom env

--- tests/TokensRegexProvider.ProviderTypes.Tests/TokenSequencePatternProviderTests.fs
+++ tests/TokensRegexProvider.ProviderTypes.Tests/TokenSequencePatternProviderTests.fs
@@ -1,16 +1,28 @@
 module TokenSequencePatternProviderTests
 
+open edu.stanford.nlp.pipeline
+open edu.stanford.nlp.ling
 open NUnit.Framework
 open FsUnitTyped
-open Helpers
-
 open TokensRegexProvider
+
+let private cls = CoreAnnotations.TokensAnnotation().getClass()
+let private pipeline =
+    let res = new AnnotationPipeline()
+    res.addAnnotator(new TokenizerAnnotator(false, "en"))
+    res
+
+let tokenize (text:string) =
+    let annotation = new Annotation(text)
+    pipeline.annotate(annotation)
+    annotation.get(cls) :?> java.util.List
+
+let tokens = tokenize "the number is five or 5 or 5.0 or but not 5x or -5 or 5L."
 
 type FiveOrSeqPattern = TokenSequencePatternProvider<"(?$my five|5|5x|5.0|-5|5L) (?$or or)">
 
 [<Test>]
 let ``Match text by FiveOrSeqPattern`` () =
-    let tokens = tokenize "the number is five or 5 or 5.0 or but not 5x or -5 or 5L."
     let pattern = FiveOrSeqPattern()
     let matcher = pattern.GetMatcher(tokens)
 
